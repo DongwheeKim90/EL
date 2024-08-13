c75ff97a874b1c58b0ce5db890f9796e27552726
dongwhee.kim <dongwhee.kim@datastreams.co.kr>
2024-08-13 16:16:34 +0900

    add HWABAEK

--- pages/ELOBOT.py
+++ pages/ELOBOT.py
@@ -1,8 +1,11 @@
 #import method
-from pages.EL_openai_FUNC.openai_func import talkAI_EN
+from pages.EL_openai_FUNC.openai_func import talkAI_EN, generate_response
 #import library
+from PIL import Image
+import io
 import streamlit as st
 import pandas as pd
+import requests
 
 #page setting
 st.set_page_config(layout="wide")
@@ -15,42 +18,137 @@ st.markdown("In the future, we plan to release ELOBOT, created with our own tech
 
 #-Kinds of ELOBOT
 elobot_df = pd.DataFrame({
-    "Model Num" : ["EL-ELOBOT-EN-1.0", "EL-PAINTBOT-HWABAECK-1.0"],
+    "Model Num" : ["EL-ELOBOT-EN-1.0", "EL-PAINTBOT-HWABAEK-1.0"],
     "Model Name" : ["ELOBOT is English teacher", "ELBOT is nice Artist"],
     "Model Function(summary)" : ["This is a bot that answers questions about English words or sentences and corrects errors.", "This bot helps you visualize what's in your head."]
 })
 st.dataframe(elobot_df, hide_index=True, use_container_width=True, width=700)
 #body
-tab_1, tab_2 = st.tabs(["ENG Teacher", "Nice artist"]) 
+tab_1, tab_2 = st.tabs(["ENG Teacher", "Nice Artist"])
+
 # EN : OPENAI_CHAT
 with tab_1:
-    # Managing History. Initialize session state for storing chat history if it doesn't exist.
-    # if messages not in session_state, print out blank.
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = []
-    
-    # conversation area
-    message_area = st.container(height=300)
-    # --Display chat history on initial load when input the message
-    with message_area:
-        for role, message in st.session_state["messages"]:
-            # --parameter of chat_meesage is role. role include of user and assistant
-            st.chat_message(role).write(message)
-    
-    # Input area & user ask
-    st_userAsk = st.chat_input(placeholder="Please ask to ELOBOT_EN About English", key=None, max_chars=None, disabled=False, on_submit=None, args=None, kwargs=None)
-    
-    if st_userAsk:
-        # Get response from elobot : return answer for ask from openai_fun.py > talkAI_EN
-        st_gpt_response = talkAI_EN(st_userAsk)
-        
-        # Update chat history
-        # according st.seesion_STATE include of key(role), value(message)
-        st.session_state["messages"].append(('user', st_userAsk))
-        st.session_state["messages"].append(('assistant', st_gpt_response))
-        
-        # Display new messages
+    #divide layout and fix the area rate
+    EN_col_1, EN_col_2 = st.columns([1,2.8])
+    with EN_col_1:
+        st.image("useData/EL_img/ELOBOT_ENG.png", width=350)
+    with EN_col_2:
+        # Managing History. Initialize session state for storing chat history if it doesn't exist.
+        # if messages not in session_state, print out blank.
+        if "messages" not in st.session_state:
+            # make a empty list and saving message here
+            st.session_state["messages"] = []
+
+        # conversation area
+        message_area = st.container(height=300)
+        # --Display chat history on initial load when input the message
         with message_area:
-            # Display every messages of user & assistant
-            st.chat_message("user").write(st_userAsk)
-            st.chat_message("assistant").write(st_gpt_response)
+            for role, message in st.session_state["messages"]:
+                # --parameter of chat_meesage is role. role include of user and assistant
+                st.chat_message(role).write(message)
+        
+
+        # Input area & user ask
+        st_userAsk = st.chat_input(placeholder="Please ask to ELOBOT_EN About English", key=None, max_chars=None, disabled=False, on_submit=None, args=None, kwargs=None)
+        
+        if st_userAsk:
+            # Get response from elobot : return answer for ask from openai_fun.py > talkAI_EN
+            st_gpt_response = talkAI_EN(st_userAsk)
+            
+            # Update chat history
+            # according st.seesion_STATE include of key(role), value(message)
+            st.session_state["messages"].append(('user', st_userAsk))
+            st.session_state["messages"].append(('assistant', st_gpt_response))
+            
+            # Display new messages
+            with message_area:
+                # Display every messages of user & assistant
+                st.chat_message("user").write(st_userAsk)
+                st.chat_message("assistant").write(st_gpt_response)
+# NA : OPENAI_CHAT                
+with tab_2:
+
+    hwabaek_col_1, hwabaek_col_2, hwabaek_col_3, hwabaek_col_4 = st.columns([0.1,0.45,1,0.1]) 
+
+    with hwabaek_col_1:
+        st.markdown("#")
+
+    with hwabaek_col_2:
+        st.image("useData/EL_img/ELOBOT_HWABAEK.png", width=350)
+
+    with hwabaek_col_3:
+        st.markdown("### You can select type of ELOBOT for drawing image.")             
+        st.markdown("##### (1)The HWABAEK-ELOBOT")        
+        st.markdown("##### The HWABAEK-ELOBOT is drawing what your thinking or imagination. \
+                    If you want to get Nice Result? Enter your expression in details by using words or sentences. \
+                    Then your imagination will become to materialize and reality.")           
+        st.markdown("##### (2)JAVIS-ELOBOT")
+        st.markdown("##### The JAVIS-ELOBOT can drawing that New Products with slogan.\
+                    For example, If your company prepare to new product design planning or before launch? \
+                    JAVIS-ELBOT can helpful to you. JAVIS-ELOBOT can your own secretary.")
+        st.markdown("##### Draw your own image and see your imagination and thoughts together!")
+
+    with hwabaek_col_4:
+        st.markdown("#")        
+
+    #Describe & Setting area for drawing images.
+    kinds_NA_ELOBOT = ["HWABAEK : The HWABAEK-ELOBOT is drawing what your thinkin or imagination.",
+                       "JAVIS-ELOBOT : The JAVIS-ELOBOT is drawing that New Products with slogan."]
+            
+    option = st.selectbox(
+        "Please select ELOBOT option for your purpose.",
+        (kinds_NA_ELOBOT)
+    )
+    print(option)
+    for v in range(len(kinds_NA_ELOBOT)):
+
+        if option == kinds_NA_ELOBOT[v]:
+            drawing_area_col_1,drawing_area_col_2 = st.columns([1.5,1])
+            with drawing_area_col_1:
+                
+                st.title("The HWABAEK-ELOBOT ZONE!!")
+                st.markdown("### ")
+                prompt = st.text_input("1.Please enter detailed words or sentences for high-quality","Here is text-input area.")
+                imgtype = st.radio("2.Please select type of image.",
+                    ["natural", "vivid"])
+                kinds_of_size = ["1024x1024","1024x1792","1792x1024"]
+                imgsize = st.selectbox(
+                    "3.Please select size of image",
+                    (kinds_of_size)
+                )
+                st.markdown("### ")
+
+                HWABAEK_ACTION_BTN = st.button("Drawing Image", use_container_width=True, type="primary")
+                HWABAEK_RESULT = None
+                if HWABAEK_ACTION_BTN:
+                    HWABAEK_RESULT = generate_response(prompt, imgtype, imgsize)
+
+            with drawing_area_col_2:
+                if HWABAEK_RESULT:
+                    
+                    # URL에서 이미지를 가져오기
+                    response = requests.get(HWABAEK_RESULT) 
+                    
+                    # 이미지 데이터를 바이너리 형식으로 변환
+                    img = Image.open(io.BytesIO(response.content))
+                    
+                    # 이미지를 화면에 표시
+                    with st.container(height=450, border=True):
+                        st.image(img, use_column_width=True)
+                    
+                    # 이미지를 바이트 배열로 변환
+                    img_byte_arr = io.BytesIO()
+                    img.save(img_byte_arr, format='PNG')
+                    img_byte_arr = img_byte_arr.getvalue()
+                    
+                    # 다운로드 버튼 제공
+                    st.download_button(
+                        label="Download",
+                        data=img_byte_arr,
+                        file_name="HWABAEK_BOT_IMG.png",
+                        mime="image/png",
+                        use_container_width=True
+                    )
+                else:
+                    with st.container(height=450, border=True):
+                        st.image("useData/EL_img/HWABAEK_GUIDE.png")
--- pages/EL_openai_FUNC/openai_func.py
+++ pages/EL_openai_FUNC/openai_func.py
@@ -2,14 +2,16 @@
 import openai
 import os
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import (ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate)
 
 # API_KEY 설정
 # --Inner
-openai.api_key = os.environ.get("mySecretkey_openai")
+#openai.api_key = os.environ.get("mySecretkey_openai")
 # --streamlit
 myOpenAI_Key = st.secrets["mySecretkey_openai"]
 
-# (1) openai Chatbot : no settings
+# (1) openai Chatbot : ELBOT_EN
 def talkAI_EN(user_message):
     #user input word
     input_value = {
@@ -37,3 +39,52 @@ def talkAI_EN(user_message):
     gpt_response = response["choices"][0]["message"]["content"]
 
     return gpt_response
+
+# (2) openai Chatbot : ELBOT_HWABAEK
+def HWABAEK_TRANSLATE_PROMPT(text):
+    translate_template = "You are a helpful assistant that translates {input_language} to {output_language}."
+    system_message = translate_template.format(input_language="Korean", output_language="English")
+
+    # API 호출로 번역 결과 생성
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": text}
+        ],
+        temperature=0,
+        max_tokens=100
+    )
+    
+    return response.choices[0].message['content'].strip()
+
+def generate_response(prompt, imgtype, imgsize):
+    # 번역 결과 생성
+    translate_result = HWABAEK_TRANSLATE_PROMPT(prompt)
+
+    # GPT-3.5 응답 생성
+    HWABAEK_GPT_PROMPT = [
+        {"role": "system", "content": "Imagine the details of the input shape. Please answer briefly."},
+        {"role": "user", "content": translate_result}
+    ]
+
+    gpt_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=HWABAEK_GPT_PROMPT,
+        temperature=0.6,
+        max_tokens=150
+    )
+    gpt_prompt_result = gpt_response.choices[0].message['content'].strip()
+
+    # DALL-E 이미지 생성
+    dalle_response = openai.Image.create(
+        model="dall-e-3",
+        prompt = prompt,
+        size = imgsize,
+        n = 1,
+        style=imgtype
+    )
+
+    dalle_img_url = dalle_response['data'][0]['url']
+
+    return dalle_img_url
